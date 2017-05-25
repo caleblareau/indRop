e9a55ec15e7f3a90cfb1ee5675f41ee7d8c78253
caleblareau <caleblareau@g.harvard.edu>
2017-05-25 15:26:45 -0400

    started R package infrastructure

---
+++ R/scTools.R
@@ -0,0 +1,9 @@
+#' scTools: Efficient tools for single cell analyses
+#'
+#' Functions written by CAL to do cool stuff.
+#'
+#' @useDynLib scTools
+#' @docType package
+#' @name scTools
+#' @import Rcpp
+NULL
